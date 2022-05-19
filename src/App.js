34e4344a6c39c666cb59a22537d93860d6504e2e
Nour Ghorayeb <nour.ghorayeb99@gmail.com>
2022-05-19 15:21:15 +0300

    Fist Commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+// import Navbar from './components/Navbar/Navbar';
+// import Products from './components/Products/Products';
+
+import { Products, Navbar } from './components';
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Products />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Products/Products.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Grid } from '@material-ui/core';
+
+import Product from "./Product/Product";
+
+const products = [
+    { id: 1, name: 'Shoes', description: 'Running shoes.', price: '$5', image: 'https://m.media-amazon.com/images/I/81i057rz8gS._UX500_.jpg'},
+    { id: 2, name: 'Macbook', description: 'Apple macbook.', price: '$10', image: 'https://pngimg.com/uploads/macbook/macbook_PNG65.png '},
+]; 
+
+const Products = () => {
+    return (
+        <main>  {/* Grid of type container */}
+        <Grid container justify="center" spacing={4}>
+            {products.map((product) => ( //grid of type item
+                <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
+                    <Product product={product}/>
+                </Grid>
+            ))}
+
+        </Grid>
+    </main>
+    );
+};
+
+export default Products;
