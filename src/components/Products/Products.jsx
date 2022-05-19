a46637336e40b558ab647677ec41571592057c66
Nour Ghorayeb <nour.ghorayeb99@gmail.com>
2022-05-19 15:38:44 +0300

    Navbar Finish

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -3,21 +3,27 @@ import { Grid } from '@material-ui/core';
 
 import Product from "./Product/Product";
 
+import useStyles from './styles';
+
 const products = [
     { id: 1, name: 'Shoes', description: 'Running shoes.', price: '$5', image: 'https://m.media-amazon.com/images/I/81i057rz8gS._UX500_.jpg'},
     { id: 2, name: 'Macbook', description: 'Apple macbook.', price: '$10', image: 'https://pngimg.com/uploads/macbook/macbook_PNG65.png '},
+    { id: 3, name: 'Macbook', description: 'Apple macbook.', price: '$10', image: 'https://pngimg.com/uploads/macbook/macbook_PNG65.png '},
+    { id: 4, name: 'Macbook', description: 'Apple macbook.', price: '$10', image: 'https://pngimg.com/uploads/macbook/macbook_PNG65.png '},
 ]; 
 
 const Products = () => {
+
+    const classes = useStyles();
+
     return (
-        <main>  {/* Grid of type container */}
-        <Grid container justify="center" spacing={4}>
+        <main className={classes.content}>  {/* Grid of type container */}
+        <Grid container justify="center" spacing={4} className={classes.root}>
             {products.map((product) => ( //grid of type item
                 <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
                     <Product product={product}/>
                 </Grid>
             ))}
-
         </Grid>
     </main>
     );
--- src/components/Products/styles.js
+++ src/components/Products/styles.js
@@ -5,9 +5,13 @@ export default makeStyles((theme) => ({
   content: {
     flexGrow: 1,
     backgroundColor: theme.palette.background.default,
-    padding: theme.spacing(3),
+    //padding: theme.spacing(3),
+    paddingTop: '80px'
   },
   root: {
     flexGrow: 1,
   },
+  // grid: {
+  //   paddingTop: '80px'
+  // }
 }));
